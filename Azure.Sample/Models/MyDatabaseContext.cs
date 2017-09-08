38f9a8b4c2a01f56584204d474a3df4a509fb2f6
Marcin Hoppe <marcin.hoppe@particular.net>
2017-09-08 16:43:39 +0200

    Rename connection string

--- Azure.Sample/Models/MyDatabaseContext.cs
+++ Azure.Sample/Models/MyDatabaseContext.cs
@@ -11,7 +11,7 @@ namespace Azure.Sample.Models
         // For more information refer to the documentation:
         // http://msdn.microsoft.com/en-us/data/jj591621.aspx
 
-        public MyDatabaseContext() : base("name=MyDbConnection")
+        public MyDatabaseContext() : base("name=AzureSampleConnectionString")
         {
         }
 
